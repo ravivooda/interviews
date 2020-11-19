d5d166305eac998ffbee43be63556f7ad4067502
Ravi Vooda <rvooda@uber.com>
2020-11-18 20:28:40 -0800

    Scaffolding coredata project

---
+++ iOS/CoreData/CoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreData
+//
+//  Created by Ravi Vooda on 11/18/20.
+//  Copyright © 2020 Ravi Vooda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
